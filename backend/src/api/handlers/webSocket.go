a8feae8657e26ba4558c09b707ba9e19ffd08501
AndersLH <andelha@stud.ntnu.no>
2023-08-28 12:34:03 +0200

    Added random adjective to Bobby's name, with support for nouns as well

--- backend/src/api/handlers/webSocket.go
+++ backend/src/api/handlers/webSocket.go
@@ -2,13 +2,23 @@ package handlers
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"mines/src/api/errorHandler"
 	"mines/src/mines"
 	"mines/structs"
 	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+var adjectives = readFromFile("./src/api/handlers/adjectives.txt", 1532)
+
+//var nouns = readFromFile("./src/api/handlers/nouns.txt", 1323)
 
 var wsUpgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -26,11 +36,34 @@ func ConnectWS(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Find random words from slices
+	rand.Seed(time.Now().UnixNano())
+	ranAdj := rand.Intn(1333)
+	//ranNoun := rand.Intn(1524)
+	adj := adjectives[ranAdj]
+	//noun := nouns[ranNoun]
+
 	player := structs.Player{
-		Ws:   wsConnection,
-		Name: fmt.Sprintf("bobby%d", rand.Intn(100)),
+		Ws: wsConnection,
+		//Name: fmt.Sprintf(("%s%s"), strings.TrimSpace(strings.Title(adj)), strings.Title(noun)),
+		Name: fmt.Sprintf(("%sBobby"), strings.TrimSpace(strings.Title(adj))),
 	}
 
 	mines.Players = append(mines.Players, &player)
 	mines.SocketListener(&player)
+}
+
+//Read file
+func readFromFile(filename string, lines int) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	readWord, _ := ioutil.ReadFile(filename)
+	allWord := string(readWord)
+	listWord := strings.Split(allWord, "\n")
+
+	return listWord
 }
